6a0e533a19de278f1616d7bba8980aad72eb477e
Francis Li <mail@francisli.com>
2019-08-27 21:22:26 -0700

    [#5] Flip axes of polls table

--- client/home/polls.component.ts
+++ client/home/polls.component.ts
@@ -15,6 +15,8 @@ import find from 'lodash/find';
 export class PollsComponent {
   @Input() state: string = null;
   polls: any[] = null;
+  rankings: any[] = null;
+  numVisible = 10;
 
   constructor(private api: ApiService) {}
 
@@ -25,6 +27,18 @@ export class PollsComponent {
     }
     this.api.polls.index(params).subscribe(response => {
       this.polls = response.body;
+      if (this.polls.length > 0) {
+        let rankings = cloneDeep(this.polls[0].averages);
+        //// collect poll percentages for each ranked candidate
+        for (let poll of this.polls) {
+          for (let ranking of rankings) {
+            let answer = this.answerFor(ranking, poll.answers);
+            ranking.polls = ranking.polls || [];
+            ranking.polls.push(answer.pct);
+          }
+        }
+        this.rankings = rankings;
+      }
     });
   }
 
